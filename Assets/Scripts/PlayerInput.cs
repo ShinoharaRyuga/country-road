04256cd450ba9358f88d5176959749328e94290e
SinoharaRyuga <raitan3170@gmail.com>
2022-09-18 06:11:20 +0900

    最低限の動きを作成 #15

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -8,63 +8,18 @@ public class PlayerInput : MonoBehaviour
 {
     /// <summary>交換したいタイル </summary>
     [SerializeField] TileBase _startMapTile = default;
-    /// <summary>交換先したいタイル </summary>
+    /// <summary>交換先のタイル </summary>
     [SerializeField] TileBase _endMapTile = default;
-    [SerializeField] GameObject _gameObject;
     Vector3 _startPoint = default;
     Vector3 _endPoint = default;
 
-    Vector3 _point = Vector3.zero;
-    GameObject go = null;
+    // 位置座標
+    private Vector3 position;
+
+    GameObject go;
+    Vector3 _point;
     void Update()
     {
-        //foreach (var touch in Input.touches)    //スワイプ入力
-        //{
-        //    if (touch.phase == TouchPhase.Began)
-        //    {
-        //        Ray ray = Camera.main.ScreenPointToRay(touch.position);
-        //        RaycastHit hit;
-
-        //        if (Physics.Raycast(ray, out hit))
-        //        {
-        //            if (hit.collider.gameObject.CompareTag("Tile"))
-        //            {
-        //                _startMapTile = hit.collider.gameObject.GetComponent<MapTile>();
-        //                _startPoint = _startMapTile.transform.position;
-        //                Debug.Log($"start {_startMapTile.name}");
-        //            }
-        //        }
-        //    }
-        //    else if (touch.phase == TouchPhase.Ended)
-        //    {
-        //        Ray ray = Camera.main.ScreenPointToRay(touch.position);
-        //        RaycastHit hit;
-
-        //        if (Physics.Raycast(ray, out hit))
-        //        {
-        //            if (hit.collider.gameObject.CompareTag("Tile"))
-        //            {
-        //                if (_startMapTile != hit.collider.gameObject.GetComponent<MapTile>())
-        //                {
-        //                    _endMapTile = hit.collider.gameObject.GetComponent<MapTile>();
-        //                }
-
-        //            }
-        //        }
-
-        //if (_startMapTile != null && _endMapTile != null)
-        //{
-        //    _startMapTile.transform.position = _endPoint;
-        //    _endMapTile.transform.position = _startPoint;
-
-
-        //}
-
-        //_startMapTile = null;
-        //_endMapTile = null;
-        //    }
-        //}
-
         //マウスで操作
         if (Input.GetButtonDown("Fire1"))
         {
@@ -75,7 +30,7 @@ public class PlayerInput : MonoBehaviour
                 if (hit.collider.gameObject.CompareTag("Tile") && _startMapTile == null)
                 {
                     _startMapTile = hit.collider.gameObject.GetComponent<TileBase>();
-                    _startPoint = _startMapTile.transform.position;
+                   // _startPoint = _startMapTile.transform.position;
                 }
             }
         }
@@ -91,12 +46,12 @@ public class PlayerInput : MonoBehaviour
                     if (_startMapTile != hit.collider.gameObject.GetComponent<TileBase>())
                     {
                         _endMapTile = hit.collider.gameObject.GetComponent<TileBase>();
-                        _endPoint = _endMapTile.transform.position;
+                       // _endPoint = _endMapTile.transform.position;
                     }
                 }
             }
 
-          
+
 
             if (_startMapTile != null && _endMapTile != null)
             {
@@ -104,10 +59,6 @@ public class PlayerInput : MonoBehaviour
                 //_startMapTile.transform.position = _endPoint;
                 //  _endMapTile.transform.position = _startPoint;
 
-                //var x = (_startPoint.x + _endPoint.x) / 2;
-                //var z = (_startPoint.z + _endPoint.z) /2;
-                //var rotationPoint = new Vector3(x, 0, z);
-                //_startMapTile.transform.RotateAround(rotationPoint, Vector3.up, 360.0f / (1.0f / 0.1f) * Time.deltaTime);
                 //位置情報を入れ替える
                 var tmpStartRow = _startMapTile.Row;
                 var tmpStartCol = _startMapTile.Col;
@@ -119,23 +70,54 @@ public class PlayerInput : MonoBehaviour
             _endMapTile = null;
         }
 
-        
+
 
         if (go == null)
         {
-            var x = (_startMapTile.transform.position.x + _endMapTile.transform.position.x) / 2;
-            var z = (_startMapTile.transform.position.z + _endMapTile.transform.position.z) / 2;
-            _point = new Vector3(x, 0, z);
-            go = Instantiate(_gameObject, _point, Quaternion.identity);
+            go = Instantiate(new GameObject(), (_startMapTile.transform.position + _endMapTile.transform.position) / 2, Quaternion.identity);
             go.transform.forward = _startMapTile.transform.position - _endMapTile.transform.position;
+            _point = go.transform.position;
+            _startPoint = _startMapTile.transform.position;
+            _endPoint = _endMapTile.transform.position;
         }
         else
         {
-            _startMapTile.transform.RotateAround(_point, go.transform.right, 360.0f / (1.0f / 0.5f) * Time.deltaTime);
-            _endMapTile.transform.RotateAround(_point, go.transform.right, 360.0f / (1.0f / 0.5f) * Time.deltaTime);
-        }
-      
+            var distance = Vector3.Distance(_startMapTile.transform.position, _endPoint);
+            Debug.Log(distance);
+            if (0.05f <= distance)
+            {
+                var tr = _startMapTile.transform;
+                // 回転のクォータニオン作成
+                var angleAxis = Quaternion.AngleAxis(360 / 2 * Time.deltaTime, go.transform.right);
+                // 円運動の位置計算
+                var pos = tr.position;
 
+                pos -= _point;
+                pos = angleAxis * pos;
+                pos += _point;
+
+                tr.position = pos;
+
+
+            }
+
+            var distance1 = Vector3.Distance(_endMapTile.transform.position, _startPoint);
+            Debug.Log(distance1);
+            if (0.03f <= distance1)
+            {
+                var tr1 = _endMapTile.transform;
+                // 回転のクォータニオン作成
+                var angleAxis1 = Quaternion.AngleAxis(360 / 2 * Time.deltaTime, go.transform.right);
+                // 円運動の位置計算
+                var pos1 = tr1.position;
+
+                pos1 -= _point;
+                pos1 = angleAxis1 * pos1;
+                pos1 += _point;
+
+                tr1.position = pos1;
+            }
+        }
 
         //デバッグ用
         if (Input.GetButtonDown("Fire2"))
@@ -166,3 +148,52 @@ public class PlayerInput : MonoBehaviour
         }
     }
 }
+
+
+//foreach (var touch in Input.touches)    //スワイプ入力
+//{
+//    if (touch.phase == TouchPhase.Began)
+//    {
+//        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+//        RaycastHit hit;
+
+//        if (Physics.Raycast(ray, out hit))
+//        {
+//            if (hit.collider.gameObject.CompareTag("Tile"))
+//            {
+//                _startMapTile = hit.collider.gameObject.GetComponent<MapTile>();
+//                _startPoint = _startMapTile.transform.position;
+//                Debug.Log($"start {_startMapTile.name}");
+//            }
+//        }
+//    }
+//    else if (touch.phase == TouchPhase.Ended)
+//    {
+//        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+//        RaycastHit hit;
+
+//        if (Physics.Raycast(ray, out hit))
+//        {
+//            if (hit.collider.gameObject.CompareTag("Tile"))
+//            {
+//                if (_startMapTile != hit.collider.gameObject.GetComponent<MapTile>())
+//                {
+//                    _endMapTile = hit.collider.gameObject.GetComponent<MapTile>();
+//                }
+
+//            }
+//        }
+
+//if (_startMapTile != null && _endMapTile != null)
+//{
+//    _startMapTile.transform.position = _endPoint;
+//    _endMapTile.transform.position = _startPoint;
+
+
+//}
+
+//_startMapTile = null;
+//_endMapTile = null;
+//    }
+//}
+
